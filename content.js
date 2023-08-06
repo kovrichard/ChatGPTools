b568728136a9ea5060f9e24be878a30788594109
Richard Kovacs <richardkovacs.it@gmail.com>
2023-08-06 23:59:55 +0200

    Initial commit

---
+++ content.js
@@ -0,0 +1,20 @@
+document.addEventListener('keydown', (event) => {
+	if (event.target.id === 'prompt-textarea' && event.key === 'ArrowUp') {
+		event.preventDefault();
+
+		const elements = document.querySelectorAll('.empty\\:hidden');
+		const lastPrompt = elements[elements.length - 1].innerText;
+
+		const textarea = document.getElementById('prompt-textarea');
+		textarea.value = lastPrompt;
+		textarea.focus();
+	}
+
+	if (event.target.id === 'prompt-textarea' && event.key === 'ArrowDown') {
+		event.preventDefault();
+
+		const textarea = document.getElementById('prompt-textarea');
+		textarea.value = '';
+		textarea.focus();
+	}
+});
